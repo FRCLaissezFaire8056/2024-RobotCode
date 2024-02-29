4b563b027e49cad6f30c6a0564a6b5312f17aef3
Enes <enes.turkkan07@gmail.com>
2024-02-29 19:41:58 +0300

    commit

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,10 +1,15 @@
 package frc.robot.subsystems;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.numbers.N5;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.util.Units;
 
 import org.photonvision.PhotonCamera;
 
@@ -17,6 +22,21 @@ public class VisionSubsystem extends SubsystemBase{
     private SwerveDrivePoseEstimator poseEstimator;
     private DriveSubsystem driveSubsystem;
 
+
+
+    public Vector<N3> createStateStdDevs(double x, double y, double theta) {
+        return VecBuilder.fill(x, y, Units.degreesToRadians(theta));
+    }
+
+    public Vector<N5> createLocalMeasurementStdDevs(double theta, double s) {
+        return VecBuilder.fill(Units.degreesToRadians(theta), s, s, s, s);
+    }
+
+    public Vector<N3> createVisionMeasurementStdDevs(double x, double y, double theta) {
+        return VecBuilder.fill(x, y, Units.degreesToRadians(theta));
+    }
+    
+
     public VisionSubsystem(DriveSubsystem driveSubsystem){
         this.driveSubsystem = driveSubsystem;
 
@@ -24,8 +44,12 @@ public class VisionSubsystem extends SubsystemBase{
             DriveConstants.kDriveKinematics, 
             driveSubsystem.m_gyro.getRotation2d(), 
             driveSubsystem.getModulePositions(), 
-            driveSubsystem.getPose()
+            driveSubsystem.getPose(),
+            createStateStdDevs(.1, .1, 10),
+            createVisionMeasurementStdDevs(5, 5, 500)
         );
     }
+
+
 
 }
